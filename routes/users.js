2b742c7e6e2cb4387b419f1512570b6cc13bc234
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2019-07-30 21:51:47 -0700

    hash passwords & register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -11,7 +12,7 @@ const User = require("../models/User");
 router.post(
 	"/",
 	[
-		check("name", "name is required")
+		check("name", "Name is required")
 			.not()
 			.isEmpty(),
 		check("email", "Please include the valid email").isEmail(),
@@ -20,12 +21,37 @@ router.post(
 			"Please enter the password with 6 or more characters"
 		).isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send("passed");
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res.status(400).json({ msg: "User already exist" });
+			}
+			user = new User({
+				name,
+				email,
+				password
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+            
+            res.send('User saved')
+
+		} catch(err) {
+            console.log(err.msg);
+            res.status(500).send('Server error')
+        }
 	}
 );
 
