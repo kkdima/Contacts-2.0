e66ae39a4a11adedcd607938062018384c6e3fd5
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2019-08-04 07:52:19 -0700

    Alert context, state & component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+	const alertContext = useContext(AlertContext);
+
+	const { setAlert } = alertContext;
+	
 	const [user, setUser] = useState({
 		name: "",
 		email: "",
@@ -12,9 +17,15 @@ const Register = () => {
     const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
     
     const onSubmit = e => {
-        e.preventDefault();
-        console.log('Register submit');
-        
+		e.preventDefault();
+
+		if (name === '' || email === '' || password === '') {
+			setAlert('Please enter all fields', 'danger');
+		} else if (password !== password2) {
+			setAlert('Passwords do not match', 'danger');
+		} else {
+			console.log('register submit');
+		}
     }
 
 	return (
@@ -25,12 +36,12 @@ const Register = () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label htmlFor='name'>Name</label>
-					<input type='text' name='name' value={name} onChange={onChange} />
+					<input type='text' name='name' value={name} onChange={onChange} required/>
 				</div>
 
 				<div className='form-group'>
 					<label htmlFor='email'>Email address</label>
-					<input type='email' name='email' value={email} onChange={onChange} />
+					<input type='email' name='email' value={email} onChange={onChange} required/>
 				</div>
 
 				<div className='form-group'>
@@ -40,6 +51,8 @@ const Register = () => {
 						name='password'
 						value={password}
 						onChange={onChange}
+						required
+						minLength='6'
 					/>
 				</div>
 
@@ -50,6 +63,8 @@ const Register = () => {
 						name='password2'
 						value={password2}
 						onChange={onChange}
+						required
+						minLength='6'
 					/>
 				</div>
 				<input
